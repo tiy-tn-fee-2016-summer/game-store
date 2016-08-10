905f950d1afcb5ba0e089dc46cd9d4c7d3958213
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-10 08:18:12 -0500

    Update project name

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'restaurant',
+    modulePrefix: 'game-store',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
