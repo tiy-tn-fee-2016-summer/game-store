f3f706f0bc0a5ce86b579870f47d031c3bd09949
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-10 13:48:09 -0500

    Update application adapters and authenticators

---
+++ app/application/adapter.js
@@ -0,0 +1,17 @@
+import DS from 'ember-data';
+import config from 'game-store/config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:token',
+  host: config.DS.host,
+  namespace: config.DS.namespace,
+
+  urlForCreateRecord(modelName) {
+    if (modelName === 'user') {
+      return this._super(...arguments).replace('users', 'register');
+    }
+
+    return this._super(...arguments);
+  },
+});
