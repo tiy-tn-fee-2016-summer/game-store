eea0ea27d657e737c4cf5b5f99beafe0bdcc9abb
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-11 12:03:41 -0500

    feat: User is asked to add to cart

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  dialog: Ember.inject.service(),
   cart: Ember.inject.service('cart'),
   actions: {
     addToCart(game) {
-      this.get('cart').addToCart(game);
+      this.get('dialog').createDialog({
+        message: 'Are you sure?',
+      }).then(() => {
+        this.get('cart').addToCart(game);
+      });
     },
   },
 });
