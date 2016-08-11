d15374ae841281cca5e0d6e42fa198db00277f67
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-11 12:12:01 -0500

    User can see alerts and dialogs

--- app/index/controller.js
+++ app/index/controller.js
@@ -2,13 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   dialog: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
   cart: Ember.inject.service('cart'),
+
   actions: {
     addToCart(game) {
       this.get('dialog').createDialog({
         message: 'Are you sure?',
       }).then(() => {
         this.get('cart').addToCart(game);
+      }).catch(() => {
+        this.get('flashMessages').warning('We\'re so sorry to hear that...');
       });
     },
   },
